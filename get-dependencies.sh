f8b3c40fafee2917c344f9a62eb1bf5807465928
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-03-22 17:21:56 -0400

    sort packages

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,26 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm tauon-music-box pipewire-audio pipewire-jack p7zip unrar opusfile mpg123 flac libvorbis wavpack libgme libnotify python-jxlpy python-pypresence python-tekore python-plexapi python-pychromecast python-tidalapi
+pacman -Syu --noconfirm \
+	flac                \
+	libgme              \
+	libnotify           \
+	libvorbis           \
+	mpg123              \
+	opusfile            \
+	patchelf            \
+	p7zip               \
+	pipewire-audio      \
+	pipewire-jack       \
+	python-jxlpy        \
+	python-plexapi      \
+	python-pychromecast \
+	python-pypresence   \
+	python-tekore       \
+	python-tidalapi     \
+	tauon-music-box     \
+	unrar               \
+	wavpack
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
